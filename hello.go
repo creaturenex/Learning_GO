ca3bd4a6bc083fa39f58cf16353643ccc5987c68
Oscar Romero <creaturenex@gmail.com>
2024-03-12 18:03:35 -0400

    added spanish and french as opptional lang

--- hello.go
+++ hello.go
@@ -2,15 +2,33 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHolaPrefix = "Hola, "
+const frenchBonjourPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		if name == "World" {
+			name = "Mundo"
+		}
+		return spanishHolaPrefix + name
+	} else if language == french {
+		if name == "World" {
+			name = "Monde"
+		}
+		return frenchBonjourPrefix + name
+	}
+
 	return englishHelloPrefix + name
+
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -1,17 +1,45 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish, when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "Spanish")
+		want := "Hola, Mundo"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Claude", "French")
+		want := "Bonjour, Claude"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French, when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "French")
+		want := "Bonjour, Monde"
+		fmt.Println(got)
 		assertCorrectMessage(t, got, want)
 	})
 }
